344d12ede84c28412b7f235c127c71c8f0fb30a2
peisenha <eisenhauer@policy-lab.org>
2016-12-14 18:30:55 +0100

    merging repos

---
+++ _scripts/clean.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# standard library
+import os
+
+# All files and directories that are not under version control are removed.
+os.system('git clean -f -d')
---
+++ _scripts/run.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+""" This script builds the all the submission material.
+"""
+
+# standard library
+import shutil
+import os
+
+DIRECTORIES = ['paper', 'appendix', 'letter', 'cv']
+
+for _ in range(2):
+    for dir_ in DIRECTORIES:
+        os.chdir(dir_)
+        os.system('python create.py')
+        shutil.move('main.pdf', '../' + dir_ + '.pdf')
+        os.chdir('../')
+
+# Concatenate all for the submission.
+cmd = 'pdftk paper.pdf appendix.pdf cat output eisenhauer-manuscript.pdf'
+os.system(cmd)
+
+cmd = 'pdftk letter.pdf cv.pdf cat output eisenhauer-letter.pdf'
+os.system(cmd)
+
+for dir_ in DIRECTORIES:
+    os.unlink(dir_ + '.pdf')
+
+# Cleanup subdirectories
+for dir_ in DIRECTORIES:
+    os.chdir(dir_)
+    os.system('git clean -d -f')
+    os.chdir('../')
