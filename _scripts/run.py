78a6d0d6e585629703837545b6c25713e910fc73
peisenha <eisenhauer@policy-lab.org>
2017-02-03 09:39:36 +0100

    edits

--- _scripts/run.py
+++ _scripts/run.py
@@ -6,14 +6,21 @@
 import shutil
 import os
 
-DIRECTORIES = ['paper', 'appendix', 'letter', 'cv']
+DIRECTORIES = ['paper', 'appendix', 'letter']
 
 for _ in range(2):
     for dir_ in DIRECTORIES:
+        print(dir_)
         os.chdir(dir_)
         os.system('python create.py')
         shutil.move('main.pdf', '../' + dir_ + '.pdf')
         os.chdir('../')
+
+# I need to manually add the cv.
+os.chdir('_submodules/curriculum_vitae/sources')
+os.system('pdflatex eisenhauer_cv.tex')
+shutil.move('eisenhauer_cv.pdf', '../../../cv.pdf')
+os.chdir('../../../')
 
 # Concatenate all for the submission.
 cmd = 'pdftk paper.pdf appendix.pdf cat output eisenhauer-manuscript.pdf'
@@ -22,7 +29,7 @@ os.system(cmd)
 cmd = 'pdftk letter.pdf cv.pdf cat output eisenhauer-letter.pdf'
 os.system(cmd)
 
-for dir_ in DIRECTORIES:
+for dir_ in DIRECTORIES + ['cv']:
     os.unlink(dir_ + '.pdf')
 
 # Cleanup subdirectories
